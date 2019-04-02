084cb8e1dc1f4a1a777d4d5c718efbd494ba56aa
MalyginL <lenyamalygin@gmail.com>
2019-04-03 00:26:46 +0300

    test's added, RejectionHandler added

--- src/main/scala/club/malygin/web/WebController.scala
+++ src/main/scala/club/malygin/web/WebController.scala
@@ -2,12 +2,13 @@ package club.malygin.web
 
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.{RequestEntity, StatusCodes}
-import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{MalformedRequestContentRejection, RejectionHandler, Route}
 import club.malygin.web.model.Update
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import javax.inject.{Inject, Named}
+import StatusCodes._
+
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -16,8 +17,10 @@ class WebController @Inject()(webService: WebService) extends FailFastCirceSuppo
 
   import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
+  implicit def TelegramRejectionHandler = RejectionHandler.newBuilder()
+    .handleAll[MalformedRequestContentRejection] { _ => complete(BadRequest, "You are not Telegram!") }.result()
 
-  private val statRoutes:Route = cors() {
+  private def statRoutes: Route = cors() {
     (path("statistic" / "cache") & get) {
       complete(Marshal(webService.statistic).to[RequestEntity])
     } ~
@@ -25,13 +28,14 @@ class WebController @Inject()(webService: WebService) extends FailFastCirceSuppo
         complete(Marshal(webService.getAppInfo).to[RequestEntity])
       }
   }
-  private val apiRoutes:Route = {
+
+  private def apiRoutes: Route = {
     (path("api" / "cache" / "current") & get) {
       complete(webService.currentPairs)
     }
   }
 
-  private val telegramRoutes:Route = {
+  private def telegramRoutes: Route = handleRejections(TelegramRejectionHandler) {
     (path("telegram") & post) {
       entity(as[Update])(implicit json => {
         webService.process(json)
@@ -39,7 +43,6 @@ class WebController @Inject()(webService: WebService) extends FailFastCirceSuppo
       })
     }
   }
-  val routes:Route = telegramRoutes ~ statRoutes ~ apiRoutes
 
-
+  def routes: Route = telegramRoutes ~ statRoutes ~ apiRoutes
 }
--- src/test/scala/club/malygin/web/WebControllerTest.scala
+++ src/test/scala/club/malygin/web/WebControllerTest.scala
@@ -1,21 +1,26 @@
 package club.malygin.web
 
+import akka.http.scaladsl.model.StatusCodes.BadRequest
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import club.malygin.data.appStat.AppStatModel
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 import club.malygin.data.cache.CacheStatModel
-
+import org.scalatest.concurrent.ScalaFutures
 
 class WebControllerTest extends FlatSpec
   with Matchers
   with ScalatestRouteTest
-  with MockFactory {
+  with MockFactory
+  with ScalaFutures {
+
 
   val mockedService = stub[WebService]
   val routes: Route = new WebController(mockedService).routes
   (mockedService.statistic _).when().returns(CacheStatModel(1.0, 1.0, 1L, 1L))
+
   (mockedService.getAppInfo _).when().returns(AppStatModel(
     javaVendor = "test",
     javaVersion = "test",
@@ -33,21 +38,38 @@ class WebControllerTest extends FlatSpec
     daemons = 1
   ))
 
+  val testMap: collection.concurrent.Map[Long, Long] = scala.collection.concurrent.TrieMap(2233L -> 3322L)
+  (mockedService.currentPairs _).when().returns(testMap)
 
-  "get request /statistic/cache" should "return CacheStatModel" in {
-    Get("/statistic/cache") ~> Route.seal(routes) ~> check {
+  "routes" should "work" in {
+    Get("/statistic/cache") ~> routes ~> check {
       responseAs[String] shouldEqual """{"averageLoadPenalty":1.0,"hitRate":1.0,"evictionCount":1,"estimatedSize":1}"""
     }
-  }
-/*
-  "get request /statistic/app" should "return AppStatModel" in {
-    Get("/statistic/app") ~> Route.seal(routes) ~> check {
+
+    Get("/statistic/app") ~> routes ~> check {
       responseAs[String] shouldEqual
         """{"javaVendor":"test","javaVersion":"test","pid":111,"uptime":1,"heapInit":1,"heapUsed":1,"heapMax":1,"heapCommited":1,"nonHeapUsed":1,"nonHeapInit":1,"nonHeapMax":1,"nonHeapCommited":1,"threadCount":1,"daemons":1}"""
     }
+    Get("/api/cache/current") ~> routes ~> check {
+      responseAs[String] shouldEqual """{"2233":3322}"""
+    }
+    Get("/api/cache/current") ~> routes ~> check {
+      responseAs[String] shouldEqual """{"2233":3322}"""
+    }
 
+    import akka.http.scaladsl.model.ContentTypes._
+    val entity = HttpEntity(`application/json`,"""{"update_id":1}""")
+
+    Post("/telegram", entity) ~> routes ~> check {
+      responseAs[HttpResponse].status shouldBe StatusCodes.OK
+    }
+
+    val failentity = HttpEntity(`application/json`,"""{"update_iddd":1}""")
+    Post("/telegram", failentity) ~> routes ~> check {
+      status shouldBe BadRequest
+    }
 
   }
-*/
+
 
 }
